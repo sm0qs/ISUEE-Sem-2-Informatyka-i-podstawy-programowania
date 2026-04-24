77bb7d204d244ccbc63b75d40ee1d7f8d1fabd7c
sm0qs <74928880+sm0qs@users.noreply.github.com>
2026-04-24 13:45:48 +0200

    feat: implement getPath function to retrieve user input for file path

--- src/t1_files/main.cpp
+++ src/t1_files/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+std::string getPath() {
+	std::string path;
+	std::cout << "Insert path: ";
+	std::cin >> path;
+	return path;
+}
+
 int main() {
-	std::cout << "Test 1 - files";
+	try {
+		std::string path = getPath();
+	} catch (const std::string &e) {
+		std::cerr << "Error: " << e << std::endl;
+	} catch (...) {
+		std::cerr << "Critical error occurred!" << std::endl;
+	}
+
 	return 0;
 }
